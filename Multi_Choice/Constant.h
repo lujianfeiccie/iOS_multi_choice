f8c56539c41c592ccc36b85fca7ac2a2252f403a
Jacky <lujianfeiccie2012@163.com>
2014-12-07 11:38:17 +0800

    fix bugs about appid

---
+++ Multi_Choice/Constant.h
@@ -0,0 +1,17 @@
+//
+//  Constant.h
+//  Multi_Choice
+//
+//  Created by mac on 14/12/7.
+//  Copyright (c) 2014年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#define GLOBAL_BGColor [UIColor colorWithRed:243.0/255.0 green:239.0/255.0 blue:236.0/255.0 alpha:100]
+
+#define GLOBAL_APP_ID 926687232
+//Test APP ID 284417350
+//MY   APP ID 926687232
+@interface Constant : NSObject
+
+@end
